a63b7272626e55c930d0c7540d53f31bf92cb3d6
Peter Law <PeterJCLaw@gmail.com>
2021-02-27 20:11:00 +0000

    Guard unicode output during deploys
    Fixes https://github.com/PeterJCLaw/srcomp-cli/issues/9.
    
    See docstring for details.

--- sr/comp/cli/deploy.py
+++ sr/comp/cli/deploy.py
@@ -1,3 +1,5 @@
+import sys
+import warnings
 from contextlib import contextmanager
 from typing import Any, Iterator, Optional, Sequence, Type
 
@@ -35,6 +37,44 @@ def exit_on_exception(
     except kind as e:
         print_fail(msg.format(e))
         exit(1)
+
+
+@contextmanager
+def guard_unicode_output(stream):
+    """
+    Cope with users environments not being able to handle unicode by softening
+    the display of characters they can't handle.
+
+    While I would ideally prefer not to have this sort of behaviour, as it can
+    happen mid-deploy (which we cannot roll back) it's far safer that we handle
+    these _somehow_ than error the deploy part-way through.
+
+    This is aimed at users running under certain Windows Subsystem for Linux
+    Operating Systems which default to a non utf-8 locale :(
+    """
+
+    encoding = stream.encoding
+
+    if encoding.lower() == 'utf-8':
+        # Happy path
+        yield
+        return
+
+    warnings.warn(
+        "Your locale does not support unicode. Some characters may not display correctly.",
+    )
+
+    orig_write = stream.write
+
+    def write(text, *a, **k):
+        text = text.encode(encoding, errors='backslashreplace').decode(encoding)
+        orig_write(text, *a, **k)
+
+    try:
+        stream.write = write
+        yield
+    finally:
+        stream.write = orig_write
 
 
 def print_fail(*args: object, **kargs: Any) -> None:
@@ -264,13 +304,14 @@ def run_deployments(args, compstate, hosts):
 def command(args):
     from sr.comp.raw_compstate import RawCompstate
 
-    compstate = RawCompstate(args.compstate, local_only=False)
-    hosts = get_deployments(compstate)
+    with guard_unicode_output(sys.stdout), guard_unicode_output(sys.stderr):
+        compstate = RawCompstate(args.compstate, local_only=False)
+        hosts = get_deployments(compstate)
 
-    require_no_changes(compstate)
-    require_valid(compstate)
+        require_no_changes(compstate)
+        require_valid(compstate)
 
-    run_deployments(args, compstate, hosts)
+        run_deployments(args, compstate, hosts)
 
 
 def add_options(parser):
